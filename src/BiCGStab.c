ac0627f431044e5319201859efb3d1d47bec2aaf
Roman Iakymchuk <roman.iakymchuk@itwm.fraunhofer.de>
2021-09-23 16:14:11 +0200

    first scratch of bicgstab implementation (a bit too slow)

---
+++ src/BiCGStab.c
@@ -0,0 +1,390 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <mkl_blas.h>
+#include <mpi.h>
+#include <hb_io.h>
+
+#include "reloj.h"
+#include "ScalarVectors.h"
+#include "SparseProduct.h"
+#include "ToolsMPI.h"
+#include "matrix.h"
+#include "common.h"
+
+// ================================================================================
+
+#define DIRECT_ERROR 0
+#define PRECOND 0
+#define VECTOR_OUTPUT 0
+
+void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, int myId) {
+    int size = mat.dim2, sizeR = mat.dim1; 
+    int IONE = 1; 
+    double DONE = 1.0, DMONE = -1.0, DZERO = 0.0;
+    int n, n_dist, iter, maxiter, nProcs;
+    double beta, tol, alpha, umbral, rho, omega, sigma;
+    double *s = NULL, *q = NULL, *r = NULL, *p = NULL, *r0 = NULL, *y = NULL;
+    double *aux = NULL;
+    double t1, t2, t3, t4;
+//#ifdef PRECOND
+//    int i, *posd = NULL;
+//    double *diags = NULL;
+//#endif
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
+    n = size; n_dist = sizeR; maxiter = 16 * size; umbral = 1.0e-8;
+    CreateDoubles (&s, n_dist);
+    CreateDoubles (&q, n_dist);
+    CreateDoubles (&r, n_dist);
+    CreateDoubles (&r0, n_dist);
+    CreateDoubles (&p, n_dist);
+    CreateDoubles (&y, n_dist);
+#ifdef DIRECT_ERROR
+    // init exact solution
+    double *res_err = NULL, *x_exact = NULL;
+    CreateDoubles (&x_exact, n_dist);
+    CreateDoubles (&res_err, n_dist);
+    InitDoubles (x_exact, n_dist, DONE, DZERO);
+#endif // DIRECT_ERROR 
+
+//#ifdef PRECOND
+//    CreateInts (&posd, n_dist);
+//    CreateDoubles (&diags, n_dist);
+//    GetDiagonalSparseMatrix2 (mat, dspls[myId], diags, posd);
+//#pragma omp parallel for
+//    for (i=0; i<n_dist; i++) 
+//        diags[i] = DONE / diags[i];
+//#endif
+    CreateDoubles (&aux, n); 
+
+#if VECTOR_OUTPUT
+    // write to file for testing purpose
+    FILE *fp;
+    if (myId == 0) {
+        char name[50];
+        sprintf(name, "%d.txt", nProcs);
+        fp = fopen(name,"w");
+    }
+#endif
+
+    iter = 0;
+    MPI_Allgatherv (x, sizeR, MPI_DOUBLE, aux, sizes, dspls, MPI_DOUBLE, MPI_COMM_WORLD);
+    InitDoubles (s, sizeR, DZERO, DZERO);
+    ProdSparseMatrixVectorByRows (mat, 0, aux, s);            			// s = A * x
+    dcopy (&n_dist, b, &IONE, r, &IONE);                                // r = b
+    daxpy (&n_dist, &DMONE, s, &IONE, r, &IONE);                        // r -= s
+//#ifdef PRECOND
+//    VvecDoubles (DONE, diags, r, DZERO, r, n_dist);                   // r = D^-1 * r
+//#else
+//    y = r; // TODO: not copying array but pointers
+//#endif
+    dcopy (&n_dist, r, &IONE, p, &IONE);                                // p = r
+    dcopy (&n_dist, r, &IONE, r0, &IONE);                               // r0 = r
+
+    double reduce[2];
+//#if PRECOND
+//    beta = ddot (&n_dist, res, &IONE, r, &IONE);
+//    tol = ddot (&n_dist, res, &IONE, res, &IONE);
+//
+//    reduce[0] = beta;
+//    reduce[1] = tol;
+//
+//    MPI_Allreduce(MPI_IN_PLACE, reduce, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+//
+//    beta = reduce[0];
+//    tol = reduce[1];
+//
+//    tol = sqrt (tol);
+//#else
+    beta = ddot (&n_dist, r, &IONE, r, &IONE);                        // tol = r' * r
+    MPI_Allreduce (MPI_IN_PLACE, &beta, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    tol = sqrt (beta);
+//#endif
+
+#ifdef DIRECT_ERROR
+    // compute direct error
+    double direct_err;
+    dcopy (&n_dist, x_exact, &IONE, res_err, &IONE);                        // res_err = x_exact
+    daxpy (&n_dist, &DMONE, x, &IONE, res_err, &IONE);                      // res_err -= x
+
+    // compute inf norm
+    direct_err = norm_inf(n_dist, res_err);
+    MPI_Allreduce(MPI_IN_PLACE, &direct_err, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    //    // compute euclidean norm
+    //    direct_err = ddot (&n_dist, res_err, &IONE, res_err, &IONE);            // direct_err = res_err' * res_err
+    //    MPI_Allreduce(MPI_IN_PLACE, &direct_err, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    //    direct_err = sqrt(direct_err);
+#endif // DIRECT_ERROR
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (myId == 0) 
+        reloj (&t1, &t2);
+
+    while ((iter < maxiter) && (tol > umbral)) {
+        MPI_Allgatherv (p, sizeR, MPI_DOUBLE, aux, sizes, dspls, MPI_DOUBLE, MPI_COMM_WORLD);
+        InitDoubles (s, sizeR, DZERO, DZERO);
+        ProdSparseMatrixVectorByRows (mat, 0, aux, s);            		// s = A * p
+
+        if (myId == 0) 
+#ifdef DIRECT_ERROR
+            printf ("%d \t %a \t %a \n", iter, tol, direct_err);
+#else        
+        printf ("%d \t %20.10e \n", iter, tol);
+#endif // DIRECT_ERROR
+
+        alpha = ddot (&n_dist, r0, &IONE, s, &IONE);                    // alpha = <r_0, r_iter> / <r_0, s>
+        MPI_Allreduce (MPI_IN_PLACE, &alpha, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        alpha = beta / alpha;
+
+        dcopy (&n_dist, r, &IONE, q, &IONE);                            // q = r
+        rho = -alpha;
+        daxpy (&n_dist, &rho, s, &IONE, q, &IONE);                    // q = r - alpha * s;
+//#ifdef PRECOND
+//        VvecDoubles (DONE, diags, res, DZERO, y, n_dist);                   // y = D^-1 * res
+//#else
+//        y = res;
+//#endif
+        // second spmv
+        MPI_Allgatherv (q, sizeR, MPI_DOUBLE, aux, sizes, dspls, MPI_DOUBLE, MPI_COMM_WORLD);
+        InitDoubles (y, sizeR, DZERO, DZERO);
+        ProdSparseMatrixVectorByRows (mat, 0, aux, y);            		// y = A * q
+
+        // omega = <q, y> / <y, y>
+        omega = ddot (&n_dist, q, &IONE, y, &IONE);
+        rho = ddot (&n_dist, y, &IONE, y, &IONE);
+        reduce[0] = omega;
+        reduce[1] = rho;
+        MPI_Allreduce(MPI_IN_PLACE, reduce, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        omega = reduce[0] / reduce[1];
+
+        // x+1 = x + alpha * p + omega * q
+        daxpy (&n_dist, &alpha, p, &IONE, x, &IONE); 
+        daxpy (&n_dist, &omega, q, &IONE, x, &IONE); 
+
+        // r+1 = q - omega * y
+        dcopy (&n_dist, q, &IONE, r, &IONE);                            // r = q
+        rho = -omega;
+        daxpy (&n_dist, &rho, y, &IONE, r, &IONE);                    // r = q - omega * y;
+
+        // rho = <r0, r+1> and tolerance
+        rho = ddot (&n_dist, r0, &IONE, r, &IONE);
+        MPI_Allreduce (MPI_IN_PLACE, &rho, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        tol = sqrt (fabs(rho)); // TODO: do we need fabs() here ?
+
+        // sigma = (alpha / omega) * <r0, r+1> / <r0, r>
+        sigma = (alpha / omega) * rho / beta;
+        beta = rho;
+       
+        // p+1 = r+1 + beta * (p - omega * s)
+        rho = -omega; 
+        daxpy (&n_dist, &rho, s, &IONE, p, &IONE);                      // p -= rho * s
+        dscal (&n_dist, &sigma, p, &IONE);                               // p = sigma * p
+        daxpy (&n_dist, &DONE, r, &IONE, p, &IONE);                     // p += r
+        
+//#if PRECOND
+//        beta = ddot (&n_dist, res, &IONE, y, &IONE);
+//        tol = ddot (&n_dist, res, &IONE, res, &IONE);
+//
+//        reduce[0] = beta;
+//        reduce[1] = tol;
+//
+//        MPI_Allreduce(MPI_IN_PLACE, reduce, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+//
+//        beta = reduce[0];
+//        tol = reduce[1];
+//
+//        tol = sqrt (tol);
+//#else
+//#endif
+
+#ifdef DIRECT_ERROR
+        // compute direct error
+        dcopy (&n_dist, x_exact, &IONE, res_err, &IONE);                        // res_err = x_exact
+        daxpy (&n_dist, &DMONE, x, &IONE, res_err, &IONE);                      // res_err -= x
+
+        // compute inf norm
+        direct_err = norm_inf(n_dist, res_err);
+        MPI_Allreduce(MPI_IN_PLACE, &direct_err, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+        //        // compute euclidean norm
+        //        direct_err = ddot (&n_dist, res_err, &IONE, res_err, &IONE);
+        //        MPI_Allreduce(MPI_IN_PLACE, &direct_err, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        //        direct_err = sqrt(direct_err);
+#endif // DIRECT_ERROR
+
+        iter++;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (myId == 0) 
+        reloj (&t3, &t4);
+
+#if VECTOR_OUTPUT
+    // print aux
+    MPI_Allgatherv (x, n_dist, MPI_DOUBLE, aux, sizes, dspls, MPI_DOUBLE, MPI_COMM_WORLD);
+    if (myId == 0) {
+        fprintf(fp, "%d ", iter);
+        for (int ip = 0; ip < n; ip++)
+            fprintf(fp, "%20.10e ", aux[ip]);
+        fprintf(fp, "\n");
+    }
+#endif
+
+    if (myId == 0) {
+        printf ("Size: %d \n", n);
+        printf ("Iter: %d \n", iter);
+        printf ("Tol: %a \n", tol);
+        printf ("Time_loop: %20.10e\n", (t3-t1));
+        printf ("Time_iter: %20.10e\n", (t3-t1)/iter);
+    }
+
+    RemoveDoubles (&aux); RemoveDoubles (&s); RemoveDoubles (&q); 
+    RemoveDoubles (&r); RemoveDoubles (&p); RemoveDoubles (&r0); RemoveDoubles (&y);
+//#ifdef PRECOND
+//    RemoveDoubles (&diags); RemoveInts (&posd); RemoveDoubles(&y);
+//#endif
+}
+
+/*********************************************************************************/
+
+int main (int argc, char **argv) {
+    int dim; 
+    double *vec = NULL, *sol1 = NULL, *sol2 = NULL;
+    int index = 0, indexL = 0;
+    SparseMatrix mat  = {0, 0, NULL, NULL, NULL}, sym = {0, 0, NULL, NULL, NULL};
+
+    int root = 0, myId, nProcs;
+    int dimL, dspL, *vdimL = NULL, *vdspL = NULL;
+    SparseMatrix matL = {0, 0, NULL, NULL, NULL};
+    double *vecL = NULL, *sol1L = NULL, *sol2L = NULL;
+
+    int mat_from_file, nodes, size_param, stencil_points;
+
+    if (argc == 3) {
+        mat_from_file = atoi(argv[2]);
+    } else {
+        mat_from_file = atoi(argv[2]);
+        nodes = atoi(argv[3]);
+        size_param = atoi(argv[4]);
+        stencil_points = atoi(argv[5]);
+    }
+
+    /***************************************/
+
+    MPI_Init (&argc, &argv);
+
+    // Definition of the variables nProcs and myId
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcs); MPI_Comm_rank(MPI_COMM_WORLD, &myId);
+    root = nProcs-1;
+    root = 0;
+
+    /***************************************/
+
+    CreateInts (&vdimL, nProcs); CreateInts (&vdspL, nProcs); 
+    if(mat_from_file) {
+        if (myId == root) {
+            // Creating the matrix
+            ReadMatrixHB (argv[1], &sym);
+            DesymmetrizeSparseMatrices (sym, 0, &mat, 0);
+            dim = mat.dim1;
+        }
+
+        // Distributing the matrix
+        dim = DistributeMatrix (mat, index, &matL, indexL, vdimL, vdspL, root, MPI_COMM_WORLD);
+        dimL = vdimL[myId]; dspL = vdspL[myId];
+    }
+    else {
+        dim = size_param * size_param * size_param;
+        int divL, rstL, i;
+        divL = (dim / nProcs); rstL = (dim % nProcs);
+        for (i=0; i<nProcs; i++) vdimL[i] = divL + (i < rstL);
+        vdspL[0] = 0; for (i=1; i<nProcs; i++) vdspL[i] = vdspL[i-1] + vdimL[i-1];
+        dimL = vdimL[myId]; dspL = vdspL[myId];
+        int band_width = size_param * (size_param + 1) + 1;
+        band_width = 100 * nodes;
+        long nnz_here = ((long) (stencil_points + 2 * band_width)) * dimL;
+        printf ("dimL: %d, nodes: %d, size_param: %d, band_width: %d, stencil_points: %d, nnz_here: %ld\n",
+                dimL, nodes, size_param, band_width, stencil_points, nnz_here);
+        allocate_matrix(dimL, dim, nnz_here, &matL);
+        generate_Poisson3D_filled(&matL, size_param, stencil_points, band_width, dspL, dimL, dim);
+
+        // To generate ill-conditioned matrices
+        double factor = 1.0e6;
+        ScaleFirstRowCol(matL, dspL, dimL, myId, root, factor);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Creating the vectors
+    if (myId == root) {
+        CreateDoubles (&vec , dim);
+        CreateDoubles (&sol1, dim);
+        CreateDoubles (&sol2, dim);
+        InitRandDoubles (vec, dim, -1.0, 1.0);
+        InitDoubles (sol1, dim, 0.0, 0.0);
+        InitDoubles (sol2, dim, 0.0, 0.0);
+    } else {
+        CreateDoubles (&vec , dim);
+        CreateDoubles (&sol2, dim);
+        InitDoubles (vec , dim, 0.0, 0.0);
+        InitDoubles (sol2, dim, 0.0, 0.0);
+    }
+    CreateDoubles (&vecL , dimL);
+    CreateDoubles (&sol1L, dimL);
+    CreateDoubles (&sol2L, dimL);
+    InitDoubles (vecL , dimL, 0.0, 0.0);
+    InitDoubles (sol1L, dimL, 0.0, 0.0);
+    InitDoubles (sol2L, dimL, 0.0, 0.0);
+
+    /***************************************/
+
+    int i, IONE = 1;
+    double beta;
+    if (myId == root) {
+        InitDoubles (vec, dim, 1.0, 0.0);
+        InitDoubles (sol1, dim, 0.0, 0.0);
+        InitDoubles (sol2, dim, 0.0, 0.0);
+    }
+    int k=0;
+    int *vptrM = matL.vptr;
+    for (int i=0; i < matL.dim1; i++) {
+        for(int j=vptrM[i]; j<vptrM[i+1]; j++) {
+            sol1L[k] += matL.vval[j];
+        }
+        k++;
+    }
+
+    MPI_Scatterv (sol2, vdimL, vdspL, MPI_DOUBLE, sol2L, dimL, MPI_DOUBLE, root, MPI_COMM_WORLD);
+
+    BiCGStab (matL, sol2L, sol1L, vdimL, vdspL, myId);
+
+    // Error computation
+    for (i=0; i<dimL; i++) sol2L[i] -= 1.0;
+
+    beta = ddot (&dimL, sol2L, &IONE, sol2L, &IONE);            
+    MPI_Allreduce (MPI_IN_PLACE, &beta, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    beta = sqrt(beta);
+    if (myId == 0) 
+        printf ("Error: %a\n", beta);
+
+    /***************************************/
+    // Freeing memory
+    RemoveDoubles (&sol2L); RemoveDoubles (&sol1L); RemoveDoubles (&vecL);
+    RemoveInts (&vdspL); RemoveInts (&vdimL); 
+    if (myId == root) {
+        RemoveDoubles (&sol2); RemoveDoubles (&sol1); RemoveDoubles (&vec);
+        RemoveSparseMatrix (&mat); RemoveSparseMatrix (&sym);
+    } else {
+        RemoveDoubles (&sol2); RemoveDoubles (&vec);
+    }
+
+    MPI_Finalize ();
+
+    return 0;
+}
+
