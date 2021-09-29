439dac352d07bbc6b60e74e2175d233b6492498c
Roman Iakymchuk <roman.iakymchuk@itwm.fraunhofer.de>
2021-09-29 17:31:58 +0200

    clean up

--- src/BiCGStab.c
+++ src/BiCGStab.c
@@ -21,8 +21,8 @@
 // ================================================================================
 
 #define DIRECT_ERROR 0
-#define PRECOND 0
-#define VECTOR_OUTPUT 0
+#define PRECOND 1
+#define VECTOR_OUTPUT 1
 #define NBFPE 8
 
 /* 
@@ -122,7 +122,7 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
     FILE *fp;
     if (myId == 0) {
         char name[50];
-        sprintf(name, "%d.txt", nProcs);
+        sprintf(name, "fpe-%d.txt", nProcs);
         fp = fopen(name,"w");
     }
 #endif
@@ -158,7 +158,6 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
     }
     MPI_Bcast(&rho, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     // ReproAllReduce -- End
-    //MPI_Allreduce (MPI_IN_PLACE, &rho, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     tol0 = sqrt (rho);
     tol = tol0;
 
@@ -197,11 +196,10 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
 #if DIRECT_ERROR
             printf ("%d \t %a \t %a \n", iter, tol, direct_err);
 #else        
-        printf ("%d \t %20.10e \n", iter, tol);
+        printf ("%d \t %a \n", iter, tol);
 #endif // DIRECT_ERROR
 
-        //alpha = ddot (&n_dist, r0, &IONE, s, &IONE);                    // alpha = <r_0, r_iter> / <r_0, s>
-        //MPI_Allreduce (MPI_IN_PLACE, &alpha, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        // alpha = <r_0, r_iter> / <r_0, s>
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, r0, s, &fpe[0]);
         // ReproAllReduce -- Begin
         if (myId == 0) {
@@ -231,9 +229,6 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
         ProdSparseMatrixVectorByRows (mat, 0, aux, y);            		// y = A * q
 
         // omega = <q, y> / <y, y>
-//        reduce[0] = ddot (&n_dist, q, &IONE, y, &IONE);
-//        reduce[1] = ddot (&n_dist, y, &IONE, y, &IONE);
-//        MPI_Allreduce(MPI_IN_PLACE, reduce, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, q, y, &fpe[0]);
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, y, y, &fpe_tol[0]);
         // ReproAllReduce -- Begin
@@ -269,9 +264,6 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
         
         // rho = <r0, r+1> and tolerance
         // TODO: can we just use <r0, r> as the stopping criteria although it is slower converging than <r, r>
-//        reduce[0] = ddot (&n_dist, r0, &IONE, r, &IONE);
-//        reduce[1] = ddot (&n_dist, r, &IONE, r, &IONE);
-//        MPI_Allreduce (MPI_IN_PLACE, reduce, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, r0, r, &fpe[0]);
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, r, r, &fpe_tol[0]);
         // ReproAllReduce -- Begin
@@ -333,10 +325,10 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
     // print aux
     MPI_Allgatherv (x, n_dist, MPI_DOUBLE, aux, sizes, dspls, MPI_DOUBLE, MPI_COMM_WORLD);
     if (myId == 0) {
-        fprintf(fp, "%d ", iter);
+        fprintf(fp, "%d\n", iter);
         for (int ip = 0; ip < n; ip++)
-            fprintf(fp, "%20.10e ", aux[ip]);
-        fprintf(fp, "\n");
+            fprintf(fp, "%a\n", aux[ip]);
+        fclose(fp);
     }
 #endif
 
@@ -475,8 +467,6 @@ int main (int argc, char **argv) {
     // Error computation
     for (i=0; i<dimL; i++) sol2L[i] -= 1.0;
 
-//    beta = ddot (&dimL, sol2L, &IONE, sol2L, &IONE);            
-//    MPI_Allreduce (MPI_IN_PLACE, &beta, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     std::vector<double> fpe(NBFPE);
     exblas::cpu::exdot<double*, double*, NBFPE> (dimL, sol2L, sol2L, &fpe[0]);
 
