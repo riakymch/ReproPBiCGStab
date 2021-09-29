3ad0e50552217a9526906bda728b2dfb27cd36ac
Roman Iakymchuk <roman.iakymchuk@itwm.fraunhofer.de>
2021-09-30 00:09:39 +0200

    Updated init of b and computation of true residual

--- src/BiCGStab.c
+++ src/BiCGStab.c
@@ -21,7 +21,7 @@
 
 #define DIRECT_ERROR 0
 #define PRECOND 1
-#define VECTOR_OUTPUT 1
+#define VECTOR_OUTPUT 0
 
 void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, int myId) {
     int size = mat.dim2, sizeR = mat.dim1; 
@@ -39,7 +39,7 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
 #endif
 
     MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
-    n = size; n_dist = sizeR; maxiter = 16 * size; umbral = 1.0e-6;
+    n = size; n_dist = sizeR; maxiter = 16 * size; umbral = 1.0e-8;
     CreateDoubles (&s, n_dist);
     CreateDoubles (&q, n_dist);
     CreateDoubles (&r, n_dist);
@@ -403,6 +403,8 @@ int main (int argc, char **argv) {
         for(int j=vptrM[i]; j<vptrM[i+1]; j++) {
             sol1L[k] += matL.vval[j];
         }
+        // b = Ax_c, x_c = 1/sqrt(nbrows)
+        sol1L[k] = sol1L[k] / sqrt(dim);
         k++;
     }
 
@@ -410,12 +412,20 @@ int main (int argc, char **argv) {
 
     BiCGStab (matL, sol2L, sol1L, vdimL, vdspL, myId);
 
-    // Error computation
-    for (i=0; i<dimL; i++) sol2L[i] -= 1.0;
+    // Error computation ||b-Ax||
+    // case with x_exact = {1.0}
+//    for (i=0; i<dimL; i++)
+//        sol2L[i] -= 1.0;
+//    beta = ddot (&dimL, sol2L, &IONE, sol2L, &IONE);            
+    MPI_Allgatherv (sol2L, dimL, MPI_DOUBLE, sol2, vdimL, vdspL, MPI_DOUBLE, MPI_COMM_WORLD);
+    InitDoubles (sol2L, dimL, 0, 0);
+    ProdSparseMatrixVectorByRows (matL, 0, sol2, sol2L);
+    double DMONE = -1.0;
+    daxpy (&dimL, &DMONE, sol2L, &IONE, sol1L, &IONE);          
 
     // ReproAllReduce -- Begin
     std::vector<int64_t> h_superacc(exblas::BIN_COUNT);
-    exblas::cpu::exdot (dimL, sol2L, sol2L, &h_superacc[0]);
+    exblas::cpu::exdot (dimL, sol1L, sol1L, &h_superacc[0]);
     int imin=exblas::IMIN, imax=exblas::IMAX;
     exblas::cpu::Normalize(&h_superacc[0], imin, imax);
     if (myId == 0) {
