e3f65135f4dbb75f5cb40159f0f1d657546c5ac6
Roman Iakymchuk <roman.iakymchuk@itwm.fraunhofer.de>
2021-10-05 12:36:59 +0200

    Fixed the issue with reading the non-symmetric matrix; initialize b and x as in ParCo by Cools and Vanroose;

--- src/BiCGStab.c
+++ src/BiCGStab.c
@@ -21,7 +21,7 @@
 // ================================================================================
 
 #define DIRECT_ERROR 0
-#define PRECOND 1
+#define PRECOND 0
 #define VECTOR_OUTPUT 0
 #define NBFPE 8
 
@@ -90,7 +90,7 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
 #endif
 
     MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
-    n = size; n_dist = sizeR; maxiter = 16 * size; umbral = 1.0e-8;
+    n = size; n_dist = sizeR; maxiter = 16 * size; umbral = 1.0e-6;
     CreateDoubles (&s, n_dist);
     CreateDoubles (&q, n_dist);
     CreateDoubles (&r, n_dist);
@@ -155,6 +155,7 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
     }
     if (myId == 0) {
         rho = exblas::cpu::Round<double, NBFPE> (&fpe[0]);
+        printf("rho0 = %20.10e\n", rho);
     }
     MPI_Bcast(&rho, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     // ReproAllReduce -- End
@@ -263,7 +264,7 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
         daxpy (&n_dist, &tmp, y, &IONE, r, &IONE);                      // r = q - omega * y;
         
         // rho = <r0, r+1> and tolerance
-        // TODO: can we just use <r0, r> as the stopping criteria although it is slower converging than <r, r>
+        // cannot just use <r0, r> as the stopping criteria since it slows the convergence compared to <r, r>
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, r0, r, &fpe[0]);
         exblas::cpu::exdot<double*, double*, NBFPE> (n_dist, r, r, &fpe_tol[0]);
         // ReproAllReduce -- Begin
@@ -287,7 +288,7 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
         MPI_Bcast(reduce, 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         // ReproAllReduce -- End
         tmp = reduce[0];
-        tol = sqrt (fabs(reduce[1])) / tol0;
+        tol = sqrt (reduce[1]) / tol0;
 
         // beta = (alpha / omega) * <r0, r+1> / <r0, r>
         beta = (alpha / omega) * (tmp / rho);
@@ -355,14 +356,14 @@ void BiCGStab (SparseMatrix mat, double *x, double *b, int *sizes, int *dspls, i
 
 int main (int argc, char **argv) {
     int dim; 
-    double *vec = NULL, *sol1 = NULL, *sol2 = NULL;
+    double *sol1 = NULL, *sol2 = NULL;
     int index = 0, indexL = 0;
     SparseMatrix mat  = {0, 0, NULL, NULL, NULL}, sym = {0, 0, NULL, NULL, NULL};
 
     int root = 0, myId, nProcs;
     int dimL, dspL, *vdimL = NULL, *vdspL = NULL;
     SparseMatrix matL = {0, 0, NULL, NULL, NULL};
-    double *vecL = NULL, *sol1L = NULL, *sol2L = NULL;
+    double *sol1L = NULL, *sol2L = NULL;
 
     int mat_from_file, nodes, size_param, stencil_points;
 
@@ -392,7 +393,7 @@ int main (int argc, char **argv) {
         if (myId == root) {
             // Creating the matrix
             ReadMatrixHB (argv[1], &sym);
-            DesymmetrizeSparseMatrices (sym, 0, &mat, 0);
+            TransposeSparseMatrices (sym, 0, &mat, 0);
             dim = mat.dim1;
         }
 
@@ -422,45 +423,28 @@ int main (int argc, char **argv) {
     MPI_Barrier(MPI_COMM_WORLD);
 
     // Creating the vectors
-    if (myId == root) {
-        CreateDoubles (&vec , dim);
-        CreateDoubles (&sol1, dim);
-        CreateDoubles (&sol2, dim);
-        InitRandDoubles (vec, dim, -1.0, 1.0);
-        InitDoubles (sol1, dim, 0.0, 0.0);
-        InitDoubles (sol2, dim, 0.0, 0.0);
-    } else {
-        CreateDoubles (&vec , dim);
-        CreateDoubles (&sol2, dim);
-        InitDoubles (vec , dim, 0.0, 0.0);
-        InitDoubles (sol2, dim, 0.0, 0.0);
-    }
-    CreateDoubles (&vecL , dimL);
+    CreateDoubles (&sol1, dim);
+    CreateDoubles (&sol2, dim);
     CreateDoubles (&sol1L, dimL);
     CreateDoubles (&sol2L, dimL);
-    InitDoubles (vecL , dimL, 0.0, 0.0);
+    InitDoubles (sol1, dim, 1.0, 0.0);
+    InitDoubles (sol2, dim, 0.0, 0.0);
     InitDoubles (sol1L, dimL, 0.0, 0.0);
     InitDoubles (sol2L, dimL, 0.0, 0.0);
 
     /***************************************/
 
-    int i, IONE = 1;
-    double beta;
-    if (myId == root) {
-        InitDoubles (vec, dim, 1.0, 0.0);
-        InitDoubles (sol1, dim, 0.0, 0.0);
-        InitDoubles (sol2, dim, 0.0, 0.0);
-    }
-    int k=0;
-    int *vptrM = matL.vptr;
-    for (int i=0; i < matL.dim1; i++) {
-        for(int j=vptrM[i]; j<vptrM[i+1]; j++) {
-            sol1L[k] += matL.vval[j];
-        }
-        // b = Ax_c, x_c = 1/sqrt(nbrows)
-        sol1L[k] = sol1L[k] / sqrt(dim);
-        k++;
-    }
+    int IONE = 1;
+//    for (int i=0; i < matL.dim1; i++) {
+//        for(int j=vptrM[i]; j<vptrM[i+1]; j++) {
+//            sol1L[k] += matL.vval[j];
+//        }
+//    }
+
+    // compute b = A * x_c, x_c = 1/sqrt(nbrows)
+    double beta = 1.0 / sqrt(dim);
+    ProdSparseMatrixVectorByRows (matL, 0, sol1, sol1L);            			// s = A * x
+    dscal (&dimL, &beta, sol1L, &IONE);                                         // s = beta * s
 
     MPI_Scatterv (sol2, vdimL, vdspL, MPI_DOUBLE, sol2L, dimL, MPI_DOUBLE, root, MPI_COMM_WORLD);
 
@@ -498,18 +482,19 @@ int main (int argc, char **argv) {
 
     beta = sqrt(beta);
     if (myId == 0) 
-        printf ("Error: %a\n", beta);
+        printf ("Error: %20.10e\n", beta);
 
     /***************************************/
     // Freeing memory
-    RemoveDoubles (&sol2L); RemoveDoubles (&sol1L); RemoveDoubles (&vecL);
+    RemoveDoubles (&sol1); 
+    RemoveDoubles (&sol2); 
+    RemoveDoubles (&sol1L); 
+    RemoveDoubles (&sol2L);
     RemoveInts (&vdspL); RemoveInts (&vdimL); 
     if (myId == root) {
-        RemoveDoubles (&sol2); RemoveDoubles (&sol1); RemoveDoubles (&vec);
-        RemoveSparseMatrix (&mat); RemoveSparseMatrix (&sym);
-    } else {
-        RemoveDoubles (&sol2); RemoveDoubles (&vec);
-    }
+        RemoveSparseMatrix (&mat);
+        RemoveSparseMatrix (&sym);
+    } 
 
     MPI_Finalize ();
 
